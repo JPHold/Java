e59d2e1613c2a1e95fd14ef60db3b6d907d4b9f5
budd <1208405111@qq.com>
2020-02-18 13:52:46 +0800

    增加方法带异常的继承和实现(未完成)

--- JdkBasic/src/test/java/com/budd/java/jdkBasic/exception/HelloExceptionTest.java
+++ JdkBasic/src/test/java/com/budd/java/jdkBasic/exception/HelloExceptionTest.java
@@ -223,7 +223,7 @@ public class HelloExceptionTest {
      *
      * @author HJP
      * @date 2018年3月10日 上午11:36:42
-     * @Description 不是恢复模型(抛出异常, 立即退出)
+     * @Description 非恢复模型(抛出异常, 立即退出)
      */
     @Test
     public void unRecoverModel() {
@@ -293,5 +293,110 @@ public class HelloExceptionTest {
             logExceptionClass.log(e);
         }
     }
+
+    /**
+     * ** ******* ******* * ******** * * * * ******** * /**
+     *
+     * @author HJP
+     * @date 2018年3月11日 上午9:58:24
+     * @Description 方法带异常的继承和实现测试
+     */
+    class BaseballException extends Exception {
+    }
+
+    class Foul extends BaseballException {
+    }
+
+    class Strike extends BaseballException {
+    }
+
+    abstract class Inning {
+        public Inning() throws BaseballException {
+        }
+        public void event() throws BaseballException {
+            // Doesn't actually have to throw anything
+        }
+        public abstract void atBat() throws Strike, Foul;
+
+        public void walk() {
+        } // Throws no checked exceptions
+    }
+
+    class StormException extends Exception {
+    }
+
+    class RainedOut extends StormException {
+    }
+
+    class PopFoul extends Foul {
+    }
+
+    interface Storm {
+        public void event() throws RainedOut;
+        public void rainHard() throws RainedOut;
+    }
+
+    public class StormyInning extends Inning implements Storm {
+        // OK to add new exceptions for constructors, but you
+        // must deal with the base constructor exceptions:
+        public StormyInning()
+                throws RainedOut, BaseballException {
+        }
+
+        public StormyInning(String s)
+                throws Foul, BaseballException {
+        }
+
+        // Regular methods must conform to base class:
+// void walk() throws PopFoul {} //Compile error
+        // Interface CANNOT add exceptions to existing
+        // methods from the base class:
+// public void event() throws RainedOut {}
+        // If the method doesn't already exist in the
+        // base class, the exception is OK:
+        public void rainHard() throws RainedOut {
+        }
+
+        // You can choose to not throw any exceptions,
+        // even if the base version does:
+        public void event() {
+        }
+
+        // Overridden methods can throw inherited exceptions:
+        public void atBat() throws PopFoul {
+        }
+
+    }
+    @Test
+    public void testExtendException(){
+        try {
+            StormyInning si = new StormyInning();
+            si.atBat();
+        } catch (PopFoul e) {
+            System.out.println("Pop foul");
+        } catch (RainedOut e) {
+            System.out.println("Rained out");
+        } catch (BaseballException e) {
+            System.out.println("Generic baseball exception");
+        }
+        // Strike not thrown in derived version.
+        try {
+            // What happens if you upcast?
+            Inning i = new StormyInning();
+            i.atBat();
+            // You must catch the exceptions from the
+            // base-class version of the method:
+        } catch (Strike e) {
+            System.out.println("Strike");
+        } catch (Foul e) {
+            System.out.println("Foul");
+        } catch (RainedOut e) {
+            System.out.println("Rained out");
+        } catch (BaseballException e) {
+            System.out.println("Generic baseball exception");
+        }
+    }
+
+
 
 }
