53a052b635469a2e3c7e61db5aa27a706432668c
budd <1208405111@qq.com>
2020-02-18 23:06:13 +0800

    增加jdk8项目， 增加内部类测试类(内部类访问外部变量的final标识在jdk8之前和之后是否需添加)

---
+++ Jdk8platform/src/test/java/com/budd/java/jdk8/classes/innerClass/HelloInnerClassTest.java
@@ -0,0 +1,27 @@
+package com.budd.java.jdk8.classes.innerClass;
+
+import org.junit.Test;
+
+/**
+ * 内部类入门研究
+ *
+ * @author budd
+ * @since 2020年2月18日 22:52:31
+ **/
+public class HelloInnerClassTest {
+    /**
+     * @return
+     * @author HJP
+     * @date 2018年1月6日 %上午11:56:08
+     * @Description JDK1.8之前，局部类操作局部变量， 需要显示添加final
+     */
+    @Test
+    public void testLocalVarFinal() {
+        int i = 0;
+        Runnable rn = new Runnable() {
+            public void run() {
+                System.out.println(i);
+            }
+        };
+    }
+}
