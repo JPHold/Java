afbe6da28c9f1de4228bf5e39adb2891d7e00672
budd <1208405111@qq.com>
2020-04-05 20:17:37 +0800

    增加并发包(volatile和synchronized待研究)

---
+++ JdkBasic/src/test/java/com/budd/java/jdkBasic/concurrent/HelloConcurrentTest.java
@@ -0,0 +1,22 @@
+package com.budd.java.jdkBasic.concurrent;
+
+import org.junit.Test;
+
+import static com.budd.java.util.Print.*;
+
+public class HelloConcurrentTest {
+
+    /**
+     * TODO start:{测试volatile}
+     */
+    /**
+     * end:{测试volatile}
+     */
+
+    /**
+     * TODO start:{测试synchronized}
+     */
+    /**
+     * end:{测试synchronized}
+     */
+}
