2e4da297dcb3b16acd2ee387ab06dc29acd7ee40
budd <1208405111@qq.com>
2020-06-29 22:34:21 +0800

    运算符入门研究

---
+++ Jdk7Platform/src/test/java/com/budd/java/jdk7/dataType/HelloOperationTest.java
@@ -0,0 +1,21 @@
+package com.budd.java.jdk7.dataType;
+
+import org.junit.Test;
+
+/**
+ * @author budd
+ * @desc 运算符入门研究
+ * @since 2020/4/13 22:47
+ **/
+public class HelloOperationTest {
+
+    /**
+     * @author budd
+     * @desc 使用“_”作为大数字的视觉分隔符，java7及以上才支持
+     * @since 2020年4月13日 22:50:10
+     */
+    @Test
+    public void testBigNumberVisionSplit(){
+        int bigNumberVisionSplit= 1_0000_0000;//一亿
+    }
+}
