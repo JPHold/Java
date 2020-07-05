08988acda743292b64bb7d094c1cd162e22f18f4
budd <1208405111@qq.com>
2020-07-05 21:16:22 +0800

    增加下划线“_”作为大数字的视觉分隔符的研究

--- Jdk7Platform/src/test/java/com/budd/java/jdk7/dataType/HelloOperationTest.java
+++ Jdk7Platform/src/test/java/com/budd/java/jdk7/dataType/HelloOperationTest.java
@@ -2,7 +2,7 @@ package com.budd.java.jdk7.dataType;
 
 import org.junit.Test;
 
-import static com.budd.java.util.Print.printf;
+import static com.budd.java.util.Print.*;
 
 /**
  * @author budd
@@ -13,12 +13,37 @@ public class HelloOperationTest {
 
     /**
      * @author budd
-     * @desc 使用“_”作为大数字的视觉分隔符，java7及以上才支持
+     * @desc 使用下划线“_”作为大数字的视觉分隔符，java7及以上才支持
      * @since 2020年4月13日 22:50:10
      */
     @Test
-    public void testBigNumberVisionSplit() {
-        int bigNumberVisionSplit = 1_0000_0000;//一亿
+    public void testUnderlineSplit() {
+        int i = 1_0000_0000;//一亿
+        printf("int类型：%s", i);
+
+        double d = 341_435_936.445_667;
+        printf("double类型：%s", d);
+
+        int binary = 0b0010_1111_1010_1111_1010_1111_1010_1111;
+        printf("二进制类型：%s", Integer.toBinaryString(binary));
+        //%n是忽略平台差异的换行符(只针对System.out.printf() 或 System.out.format()有效，System.out.println()是输出%n)
+        System.out.printf("十六进制输出：%x%n", binary);
+
+        long hex = 0x7f_e9_b7_aa;
+        printf("十六进制输出：%x%n", hex);
+
+        /**
+         * 以下是违反使用规则的用法
+         */
+        //辅助字面量：F、D 和 L的前面不能出现。报错：Illegal underscore
+        /*float errorFloat = 123_F;
+        double errorDouble = 123_D;
+        long errorLong = 123_L;
+        */
+
+        //二进制前导 b 和 十六进制 x 前后不能出现。报错：Illegal underscore
+        /*int errorBinary = 0B_0011;
+        int errorHex = 0X_7B;*/
     }
 
     /**
