80ec243b663e2b6b8e3aa109664800865bdd9c52
budd <1208405111@qq.com>
2020-07-28 23:10:51 +0800

    移位运算,全面(没搞懂)

--- JdkBasic/src/test/java/com/budd/java/jdkBasic/dataType/ProOperationTest.java
+++ JdkBasic/src/test/java/com/budd/java/jdkBasic/dataType/ProOperationTest.java
@@ -2,6 +2,8 @@ package com.budd.java.jdkBasic.dataType;
 
 import org.junit.Test;
 
+import java.util.Random;
+
 import static com.budd.java.util.Print.*;
 
 /**
@@ -122,4 +124,63 @@ public class ProOperationTest {
         printf("(使用int接收)无符号右移后：%s%n", Integer.toBinaryString(b1));
     }
 
+    /**
+     * 移位运算,全面(没搞懂)
+     */
+    @Test
+    public void moveAllBitOperation() {
+        Random rand = new Random(47);
+        int i = rand.nextInt();
+        int j = rand.nextInt();
+        printBinaryInt("-1", 65536);
+        printBinaryInt("+1", +1);
+        int maxpos = 2147483647;
+        printBinaryInt("maxpos", maxpos);
+        int maxneg = -2147483648;
+        printBinaryInt("maxneg", maxneg);
+        printBinaryInt("i", i);
+        printBinaryInt("~i", ~i);
+        printBinaryInt("-i", -i);
+        printBinaryInt("j", j);
+        printBinaryInt("i & j", i & j);
+        printBinaryInt("i | j", i | j);
+        printBinaryInt("i ^ j", i ^ j);
+        printBinaryInt("i << 5", i << 5);
+        printBinaryInt("i >> 5", i >> 5);
+        printBinaryInt("(~i) >> 5", (~i) >> 5);
+        printBinaryInt("i >>> 5", i >>> 5);
+        printBinaryInt("(~i) >>> 5", (~i) >>> 5);
+        long l = rand.nextLong();
+        long m = rand.nextLong();
+        printBinaryLong("-1L", -1L);
+        printBinaryLong("+1L", +1L);
+        long ll = 9223372036854775807L;
+        printBinaryLong("maxpos", ll);
+        long lln = -9223372036854775808L;
+        printBinaryLong("maxneg", lln);
+        printBinaryLong("l", l);
+        printBinaryLong("~l", ~l);
+        printBinaryLong("-l", -l);
+        printBinaryLong("m", m);
+        printBinaryLong("l & m", l & m);
+        printBinaryLong("l | m", l | m);
+        printBinaryLong("l ^ m", l ^ m);
+        printBinaryLong("l << 5", l << 5);
+        printBinaryLong("l >> 5", l >> 5);
+        printBinaryLong("(~l) >> 5", (~l) >> 5);
+        printBinaryLong("l >>> 5", l >>> 5);
+        printBinaryLong("(~l) >>> 5", (~l) >>> 5);
+    }
+
+    static void printBinaryInt(String s, int i) {
+        System.out.println(
+                s + ", int: " + i + ", binary:\n " +
+                        Integer.toBinaryString(i));
+    }
+
+    static void printBinaryLong(String s, long l) {
+        System.out.println(
+                s + ", long: " + l + ", binary:\n " +
+                        Long.toBinaryString(l));
+    }
 }
