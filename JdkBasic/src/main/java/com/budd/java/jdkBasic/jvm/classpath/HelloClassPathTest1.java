33f0b879c81a6b3210faf0ac9f30189b747a43e1
JpHold <1208405111@qq.com>
2020-08-24 11:56:35 +0800

    classpath入门研究

---
+++ JdkBasic/src/main/java/com/budd/java/jdkBasic/jvm/classpath/HelloClassPathTest1.java
@@ -0,0 +1,14 @@
+package com.budd.java.jdkBasic.jvm.classpath;
+
+import static com.budd.java.util.Print.printf;
+
+/**
+ * @author budd
+ * @desc CLASSPATH-入门研究（用于java -cp 指定jar包）
+ * @since 2020/8/24 11:23
+ **/
+public class HelloClassPathTest1 {
+    public static void main(String[] args) {
+        printf("执行类，通过CLASSPATH搜索查找class文件");
+    }
+}
---
+++ JdkBasic/src/test/java/com/budd/java/jdkBasic/jvm/classpath/HelloClassPathTest.java
@@ -0,0 +1,13 @@
+package com.budd.java.jdkBasic.jvm.classpath;
+import static com.budd.java.util.Print.*;
+
+/**
+ * @author budd
+ * @desc CLASSPATH-入门研究
+ * @since 2020/8/24 11:23
+ **/
+public class HelloClassPathTest {
+    public static void main(String[] args) {
+        printf("执行类，通过CLASSPATH搜索查找class文件");
+    }
+}
