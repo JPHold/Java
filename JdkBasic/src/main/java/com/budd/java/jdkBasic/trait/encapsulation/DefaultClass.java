378c997ca45b13214b84839d639b23dd8ff64215
JpHold <1208405111@qq.com>
2020-08-24 15:50:04 +0800

    主类为默认权限

---
+++ JdkBasic/src/main/java/com/budd/java/jdkBasic/trait/encapsulation/DefaultClass.java
@@ -0,0 +1,14 @@
+package com.budd.java.jdkBasic.trait.encapsulation;
+
+import static com.budd.java.util.Print.printf;
+
+/**
+ * @author budd
+ * @desc 默认权限-类1
+ * @since 2020/8/24 15:40
+ **/
+class DefaultClass {
+    DefaultClass() {
+        printf("默认权限-类1");
+    }
+}
---
+++ JdkBasic/src/test/java/com/budd/java/jdkBasic/trait/encapsulation/HelloEncapsulationTest.java
@@ -0,0 +1,25 @@
+package com.budd.java.jdkBasic.trait.encapsulation;
+
+import org.junit.Test;
+
+/**
+ * @author budd
+ * @desc 封装-入门研究
+ * @since 2020/8/24 15:38
+ **/
+public class HelloEncapsulationTest {
+
+
+    /**
+     * @Author budd
+     * @Description 默认权限的类，主类的类型可以是public和默认
+     * @Date 2020/8/24 15:45
+     * @Param []
+     * @return void
+     **/
+    @Test
+    public void testDefaultClass(){
+        new DefaultClass();
+    }
+
+}
