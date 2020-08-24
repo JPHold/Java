9993202e8e2b733b83d773b5a0a76d82b13de72a
JpHold <1208405111@qq.com>
2020-08-24 12:04:51 +0800

    classpath入门研究只保留一个。保留main.java下的

--- JdkBasic/src/main/java/com/budd/java/jdkBasic/jvm/classpath/HelloClassPathTest.java
+++ JdkBasic/src/main/java/com/budd/java/jdkBasic/jvm/classpath/HelloClassPathTest.java
@@ -7,7 +7,7 @@ import static com.budd.java.util.Print.printf;
  * @desc CLASSPATH-入门研究（用于java -cp 指定jar包）
  * @since 2020/8/24 11:23
  **/
-public class HelloClassPathTest1 {
+public class HelloClassPathTest {
     public static void main(String[] args) {
         printf("执行类，通过CLASSPATH搜索查找class文件");
     }
