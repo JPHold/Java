930d1204226521e73c754c2dd296d8066ea991bd
budd <1208405111@qq.com>
2020-11-17 21:10:15 +0800

    增加文件系统的研究-获取文件的存储位置、获取全局的文件系统

--- Jdk7Platform/src/test/java/com/budd/java/jdk7/file/pathfile/ProPathFileTest.java
+++ Jdk7Platform/src/test/java/com/budd/java/jdk7/file/pathfile/ProPathFileTest.java
@@ -1,13 +1,19 @@
 package com.budd.java.jdk7.file.pathfile;
 
-import static com.budd.java.util.Print.*;
-
 import org.junit.Test;
 
 import java.io.IOException;
 import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributeView;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.nio.file.attribute.UserPrincipalLookupService;
+import java.nio.file.spi.FileSystemProvider;
 import java.util.List;
+import java.util.Set;
+
+import static com.budd.java.util.Print.print;
+import static com.budd.java.util.Print.printf;
 
 /**
  * @author budd
@@ -76,5 +82,104 @@ public class ProPathFileTest {
             }
         });
     }
+
+    /**
+     * start：{文件系统}
+     * http://www.1024sky.cn/blog/article/4889
+     * https://www.yuque.com/ican/canjava/managing-metadata-file-file-store-attributes#PRLWz
+     */
+
+    /**
+     * 获取文件的存储位置(如设备、分区、卷，window下是卷：C盘、D盘等)的信息
+     * 下面以window为运行环境
+     *
+     * @date2020年11月15日 23:36:00
+     */
+    @Test
+    public void testFileStoreInfo() throws IOException {
+        Path filePath = Paths.get(ROOT_PATH, "t2.txt");
+        FileStore fileStore = Files.getFileStore(filePath);
+        printf("卷的名称：%s", fileStore.name());
+        printf("卷的总大小(字节)：%s", fileStore.getTotalSpace());
+        printf("卷的未使用大小(字节)：%s", fileStore.getUsableSpace());
+        printf("卷是否只读：%s", fileStore.isReadOnly());
+        printf("卷的文件系统：%s", fileStore.type());
+
+        printf("--------------------------------------------------------------------------------------------属性视图");
+
+        printf("%nFileAttributeView");
+        //获取支持属性视图
+        BasicFileAttributeView basicFileAttributeView = Files.getFileAttributeView(filePath, BasicFileAttributeView.class);
+        printf("基础属性视图(最后修改时间)：%s", basicFileAttributeView.readAttributes().lastModifiedTime());
+        //修改最后修改时间
+        basicFileAttributeView.setTimes(FileTime.fromMillis(System.currentTimeMillis()), null, null);
+        printf("基础属性视图(修改后的最后修改时间)：%s", basicFileAttributeView.readAttributes().lastModifiedTime());
+
+        printf("%nBasicFileAttributes");
+        BasicFileAttributes basicFileAttributes = Files.readAttributes(filePath, BasicFileAttributes.class);
+        printf("基础属性视图(最后修改时间)：%s", basicFileAttributes.lastModifiedTime());
+    }
+
+    /**
+     * 获取全局的文件系统
+     *
+     * @date 2020年11月17日 20:26:57
+     */
+    @Test
+    public void testGlobalFileStream() {
+        printf("系统：%s", System.getProperty("os.name"));
+        FileSystem fsys = FileSystems.getDefault();
+
+        print("---------------------------------------------------------------------------------------获取所有存储区域");
+        for (FileStore fs : fsys.getFileStores()) {
+            printf("File Store：%s", fs);
+        }
+
+        print("-----------------------------------------------------------------------------------获取所有存储区域的路径");
+        for (Path rd : fsys.getRootDirectories()) {
+            printf("Root Directory：%s", rd);
+        }
+
+        print("------------------------------------------------------------------------获取当前文件系统支持的属性视图列表");
+        Path path = Paths.get(ROOT_PATH, "t2.txt");
+        //获取当前文件系统支持的属性视图列表
+        Set<String> supportedFileAttributeViews = fsys.supportedFileAttributeViews();
+        printf("文件系统所支持的属性视图：%s", supportedFileAttributeViews);
+        //UNIX系统、LINUX等操作系统的文件系统
+        if (supportedFileAttributeViews.contains("posix")) {
+            try {
+                printf("PosixFilePermissions：%s", Files.getPosixFilePermissions(path));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        print("-----------------------------------------------------------------------------------------------基础信息");
+        printf("分隔符：%s", fsys.getSeparator());
+        printf("当前文件系统是否启用：%s", fsys.isOpen());
+        printf("当前文件系统是否只读：%s", fsys.isReadOnly());
+
+        print("---------------------------------------------------------------------用于查询用户或用户组的用户主体查询服务");
+        UserPrincipalLookupService userPrincipalLookupService = fsys.getUserPrincipalLookupService();
+        try {
+            printf("UserPrincipalLookupService：%s", userPrincipalLookupService.lookupPrincipalByName("budd"));
+            //用户不存在，报错：java.nio.file.attribute.UserPrincipalNotFoundException
+            printf("UserPrincipalLookupService：%s", userPrincipalLookupService.lookupPrincipalByName("orange"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        print("--------------------------------------------------------------文件、目录操作的底层实现-FileSystemProvider");
+        FileSystemProvider fileSystemProvider = fsys.provider();
+        try {
+            printf("判断是否隐藏：%s", fileSystemProvider.isHidden(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * end：{文件系统}
+     */
 
 }
