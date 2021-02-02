417070b8984a1b942bdf1c992746eb552b59fc96
budd <1208405111@qq.com>
2021-02-02 21:26:13 +0800

    增加数组入门研究 1、测试默认值 2、创建二维数组 3、创建三维数组

---
+++ JdkBasic/src/test/java/com/budd/java/jdkBasic/container/array/HelloArrayTest.java
@@ -0,0 +1,51 @@
+package com.budd.java.jdkBasic.container.array;
+
+import org.junit.Test;
+
+import static com.budd.java.util.Print.*;
+import java.util.Arrays;
+
+/**
+ * @author budd
+ * @desc 数组入门研究
+ * @since 2021/2/2 20:47
+ **/
+public class HelloArrayTest {
+
+    /**
+     * 2021年2月2日 21:25:38
+     * 测试默认值
+     */
+    @Test
+    public void testDefaultValue(){
+        int []a = new int[5];
+        print(Arrays.toString(a));
+
+        String[] s = new String[5];
+        print(Arrays.toString(s));
+    }
+
+    /**
+     * 2021年2月2日 20:54:21
+     * 创建二维数组
+     */
+    @Test
+    public void testCreate2Array(){
+        int[][] a = {{1,2,3},{4,5,6}};
+        print(Arrays.deepToString(a));
+    }
+
+    /**
+     * 2021年2月2日 20:55:22
+     * 创建三维数组
+     */
+    @Test
+    public void testCreate3Array(){
+        int[][][] a = {{{0,0,0,0},{0,0,0,0}},{{0,0,0,0},{0,0,0,0}}};
+        print(Arrays.deepToString(a));
+
+        print("默认值");
+        int[][][] a1 = new int[2][2][4];
+        print(Arrays.deepToString(a1));
+    }
+}
